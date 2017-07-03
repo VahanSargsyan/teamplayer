fe1384f3a20e511ef8033f22edb01c89f7c62906
Andranik Sargsyan <andraniksargsyan21@gmail.com>
2017-07-03 12:34:51 +0400

    Improved training API

--- backend/api/training.js
+++ backend/api/training.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const training = express.Router();
 
 training.get('/', async (req, res) => {
+    const { _id } = req.user;
 
-    const _id = req.user._id;
     const Employee = mongoose.model('Employee');
+
     try {
         const employees = await Employee.find({_id: {$ne: _id}}, { firstName: 1,
                                                     lastName: 1,
@@ -13,36 +14,27 @@ training.get('/', async (req, res) => {
                                                     position: 1,
                                                     hobbies: 1,
                                                     jobDescription: 1 }).exec();
-        res.status(200).json(employees);
-    } catch(e) {
-        res.status(500).json({error: "Something went wrong"});
-    }
-});
 
-training.get('/step', async (req, res) => {
+        const trainingStep = await Employee.findOne({_id}, {_id: 0, trainingStep: 1}).exec();
 
-    const _id = req.user._id;
-    const Employee = mongoose.model('Employee');
-    try {
-        const trainingStep = await Employee.findOne({_id}, { trainingStep: 1}).exec();
-        res.status(200).json(trainingStep);
+        res.status(200).json({employees, trainingStep});
     } catch(e) {
         res.status(500).json({error: "Something went wrong"});
     }
 });
 
 training.put('/step', async (req, res) => {
-    const _id = req.user._id;
+    const { _id } = req.user;
     const trainingStep = req.body.trainingStep;
 
     const Employee = mongoose.model('Employee');
+    
     try {
         const result = await Employee.update({_id}, {$set: {trainingStep}}, {upsert: true}).exec();
         res.status(200).json(req.body);
     } catch(e) {
         res.status(500).json({error: "Something went wrong"});
     }
-
 });
 
 module.exports = training;
